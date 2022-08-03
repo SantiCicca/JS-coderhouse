4fccb25edaed5b9a4a2e3c3bf02ca35ccd17b9e0
cicca16 <cicca1611@gmail.com>
2022-08-02 23:51:37 -0300

    simulador interactivo

---
+++ js/funciones.js
@@ -0,0 +1,26 @@
+function productos() {
+    debugger
+    let producto = prompt("Eliga uno de los siguientes productos a comprar: (teclado, mouse, monitor, auricular)")
+    console.log("Seleccionaste para comprar un", producto)
+    let cuota = parseInt(prompt("Eliga la cantidad de cuotas:"))
+    console.log("Seleccionaste", cuota, "cuotas")
+
+    console.log("Precio a pagar=", realizarCuota(producto, cuota))
+
+}
+
+function realizarCuota(producto, cuota) {
+
+    switch(producto) {
+        case "teclado":
+            return prod3.precio * cuota
+        case "mouse":
+            return prod2.precio * cuota
+        case "monitor":
+            return prod1.precio * cuota
+        case "auricular":
+            return prod4.precio * cuota
+    }
+    
+}
+
