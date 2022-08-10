7ca5d5584535a355d2fdab4a9c3b6c7f60814011
cicca16 <cicca1611@gmail.com>
2022-08-09 22:39:27 -0300

    array creado sobre lista de paises

--- js/funciones.js
+++ js/funciones.js
@@ -1,29 +1,13 @@
-function productos() {
-    debugger
-    let producto = prompt("Eliga uno de los siguientes productos a comprar: (teclado, mouse, monitor, auricular)")
-    console.log("Seleccionaste para comprar un", producto)
-    let cuota = parseInt(prompt("Eliga la cantidad de cuotas: 1, 3, 6 o 12"))
-    console.log("Seleccionaste", cuota, "cuotas")
+const paises = ["Argentina", "Uruguay", "Chile", "Peru"]
 
-
-    console.log("Precio a pagar=", realizarCuota(producto, cuota))
+function listarPaises() {
+    for (let i = 0; i < paises.length; i++) {
+        console.log(paises[i])
+    }
 }
 
-function realizarCuota(producto, cuota) {
-
-    switch(producto) {
-        case "teclado":
-            return prod3.precio / cuota
-        case "mouse":
-            return prod2.precio / cuota
-        case "monitor":
-            return prod1.precio / cuota
-        case "auricular":
-            return prod4.precio / cuota
-
-        default: return "Por favor, seleccione entre las opciones (teclado, mouse, monitor o auricular)"
-    } 
-
-    
+function agregarPais() {
+    let nuevoPais = prompt("Ingresa el nuevo pais:")
+        paises.push(nuevoPais)
 }
 
