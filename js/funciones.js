9a709eba50345878feeefc4bd26cfe641e547f8f
cicca16 <cicca1611@gmail.com>
2022-08-11 21:34:28 -0300

    primer pre-entrega coder JAVASCRIPT

--- js/funciones.js
+++ js/funciones.js
@@ -1,58 +1,89 @@
-// Simulador Interactivo
+const productos = [];
 
-function productos() { 
-
-    let producto = prompt("Eliga uno de los siguientes productos a comprar: (teclado, mouse, monitor, auricular)")
-    let cuota = parseInt(prompt("Eliga la cantidad de cuotas: 1, 3, 6 o 12"))
-    console.log("Seleccionaste", cuota, "cuotas")
-
-    console.log("Precio a pagar=", realizarCuota(producto, cuota))
+function creoID() {
+  return parseInt(Math.random() * 100000);
 }
 
-function realizarCuota(producto, cuota) {
-
-    switch(producto) {
-        case "teclado":
-            return prod3.precio / cuota
-        case "mouse":
-            return prod2.precio / cuota
-        case "monitor":
-            return prod1.precio / cuota
-        case "auricular":
-            return prod4.precio / cuota
-
-        default: return "Por favor, seleccione entre las opciones (teclado, mouse, monitor o auricular)"
-    }
+class Producto {
+  constructor(id, nombre, importe) {
+    this.id = creoID();
+    this.nombre = nombre;
+    this.importe = importe;
+  }
 }
 
-// Funciones de Array "Perifericos"
-
-function listarPerifericos() {
-    for (let i = 0; i < perifericos.length; i++) {
-        console.log(perifericos[i])
-    }
+function produ() {
+  productos.push(new productos(id, nombre, importe));
 }
 
-function agregarPeriferico() {
-    let nuevoPeriferico = prompt("Ingresa el nuevo Periferico a agregar:")
-    let resultado = perifericos.includes(nuevoPeriferico)
-    if (resultado === false) {
-        perifericos.push(nuevoPeriferico)
-    } else {
-        console.error("El periferico", nuevoPeriferico, "ya esta incluido en la lista")
-    }
-        
+function generarProductos() {
+  productos.push(
+    new Producto(creoID(), "Mouse Logitech Pro Series Pro X Superlight", 19.399)
+  );
+  productos.push(new Producto(creoID(), "Mouse Razer Viper Mini", 7.199));
+  productos.push(
+    new Producto(creoID(), "Mouse HyperX Pulsefire Haste negro", 10.999)
+  );
+  productos.push(
+    new Producto(
+      creoID(),
+      "Teclado bluetooth Logitech Master Series MX Keys QWERTY",
+      21.228
+    )
+  );
+  productos.push(
+    new Producto(
+      creoID(),
+      "Teclado gamer Razer BlackWidow V3 QWERTY Green",
+      25.999
+    )
+  );
+  productos.push(
+    new Producto(
+      creoID(),
+      "Teclado gamer HyperX Alloy Origins 60 QWERTY HyperX",
+      13.999
+    )
+  );
+  productos.push(
+    new Producto(
+      creoID(),
+      "pad Logitech Desk Mat Studio Series 300x700 Colores Xl",
+      3.999
+    )
+  );
+  productos.push(
+    new Producto(
+      creoID(),
+      "Pad gamer Razer Gigantus V2 de tela y caucho xxl 410mm x 940mm x 4mm",
+      8.673
+    )
+  );
+  productos.push(
+    new Producto(
+      creoID(),
+      "Pad gamer HyperX Speed Edition Fury S Pro de caucho y tela xl 420mm x 900mm x 4mm",
+      4.998
+    )
+  );
+
+  console.table(productos);
 }
 
-function quitarPeriferico() {
-    let aQuitar = prompt("Eliga el nombre del periferico a quitar:")
-    let indice = perifericos.indexOf(aQuitar)
-    if (indice !== -1) {
-        let resultado = perifericos.splice(indice, 1)
-        console.warn("Se ha eliminado", resultado, "de la lista de perifericos")
-    } else {
-        console.error("No se ha encontrado el periferico", aQuitar)
-    }
+function buscarProducto() {
+  let prod = prompt("ingrese el producto que busca:");
+  const resultado = productos.filter((elemento) =>
+    elemento.nombre.includes(prod)
+  );
+  console.table(resultado);
+}
+
+function calcularProductos() {
+  let total = productos.reduce(
+    (acumulador, producto) => acumulador + producto.importe,
+    0
+  );
+  console.log("total valor de prodctos $:", total);
 }
 
 
